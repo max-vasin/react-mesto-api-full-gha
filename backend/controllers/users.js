db8ad8a0b7a75062fc40f6d82c721e0a2c1848c1
Max Vasin <mozg.mayhem@gmail.com>
2023-09-09 21:01:35 +0300

    wip

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -117,7 +117,7 @@ const login = async (req, res, next) => {
     );
     /*---------------------*/
 
-    res.status(200).json(token);
+    res.status(200).json({ token });
   } catch (err) {
     if (err.name === 'ValidationError') {
       next(new BadRequestError('Введены некорректные данные'));
